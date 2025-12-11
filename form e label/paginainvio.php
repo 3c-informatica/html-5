35785b8f53c0eb70e36a4762fe6db79aac9c716a
unknown <e.andru.3864@vallauri.net>
2025-12-11 08:41:05 +0100

    quello che abbiamo fatto fino ad ora

---
+++ form e label/paginainvio.php
@@ -0,0 +1,8 @@
+<?php
+// Questo è un commento: questa riga non verrà eseguita dal server.
+// PHP è un linguaggio di scripting lato server, usato per creare pagine web dinamiche.
+
+// Stampa la stringa "Ciao, Mondo!" sulla pagina web.
+echo "<h1>Ciao, Mondo!</h1>";
+// Puoi anche usare il tag <br> per andare a capo, come in HTML [7].
+echo "Benvenuto nella tua prima pagina PHP!";
